3eeeeaf9c39d6e0ed5527677f7e7e74764a94422
lunnalinks <150552639+lunnalinks@users.noreply.github.com>
2023-11-11 19:04:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ client.connect(async function (err) {
     // REDIRECT
     app.get('/visit', async (req, res) => {
         const { type } = req.query;
-        const { rows } = await client.query(type == 'random' ? random_order : linear_order, [qtd])
+        const { rows } = await client.query(random_order, [qtd])
+        //const { rows } = await client.query(linear_order, [qtd])
         console.log(rows);
         if (!rows.length) return res.redirect(default_url);
         await client.query('UPDATE links SET clicks=$2 WHERE link=$1', [rows[0].link, rows[0].clicks + 1])
@@ -46,7 +47,8 @@ client.connect(async function (err) {
     });
 
     app.get('/', async (req, res) => {
-        const { rows } = await client.query(type == 'random' ? random_order : linear_order, [qtd])
+        const { rows } = await client.query(random_order, [qtd])
+        //const { rows } = await client.query(linear_order, [qtd])
         return res.json({
             count: rows.length,
             available_links: rows.map(r => (r.link))
